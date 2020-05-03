51a83359b3367bf5e292e45d375552e1258f13d0
ArakiTakaki <araki_takaki@yahoo.co.jp>
2020-05-04 00:00:07 +0900

    done B

--- src/main.rs
+++ src/main.rs
@@ -5,31 +5,27 @@ use itertools::Itertools;
 use proconio::{input, marker::*};
 use superslice::*;
 use whiteread::parse_line;
+use std::collections::HashMap;
 
+// 3 2
+// 2
+// 1 3
+// 1
+// 3
 fn main() {
-    // let (n, m, q): (usize, usize, usize) = parse_line().unwrap();
-    // let mut abcd_list: Vec<(usize, usize, usize, usize)> = Vec::with_capacity(50);
-    // for _ in 0..q {
-    //     abcd_list.push(parse_line().unwrap())
-    // }
-    // max_floor(n, m, q, abcd_list);
-     dbg!(pow(3, 3));
-     dbg!((3 as i32).pow(3));
-}
+    let (n, k): (usize, usize) = parse_line().unwrap();
+    let mut v = vec![false; n];
 
-// fn max_floor(n: usize, m: usize, q: usize, abcd_list: Vec<(usize, usize, usize, usize)>) -> i32 {
-//     dbg!(n, m, q, abcd_list);
-//     dbg!(pow(3, 3));
-//     10
-// }
-
-fn pow(i: usize, num: usize) -> usize {
-    private_pow(i, num, num)
-}
-// これができるんだったらクロージャーでいいんじゃないの
-fn private_pow(i: usize, num: usize, current: usize ) -> usize {
-    if i == 1 {
-        return num;
+    for _ in 0..k {
+        let _: usize = parse_line().unwrap();
+        let want_to_candy_list: Vec<usize> = parse_line().unwrap();
+        for candy in want_to_candy_list {
+            v[candy - 1] = true;
+        }
     }
-    private_pow(i - 1, num * current, current)
+    let mut count: usize = 0;
+    for is_get in v {
+        count += if is_get { 0 } else { 1 };
+    }
+    println!("{}", count);
 }
