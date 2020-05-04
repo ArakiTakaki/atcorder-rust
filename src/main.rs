63a29688fec5bd57328c009fc90a852d5bc7c55d
ArakiTakaki <araki_takaki@yahoo.co.jp>
2020-05-05 02:38:15 +0900

    done

--- src/main.rs
+++ src/main.rs
@@ -7,29 +7,15 @@ use superslice::*;
 use whiteread::parse_line;
 
 fn main() {
-    // let (n, m, q): (usize, usize, usize) = parse_line().unwrap();
-    // let mut abcd_list: Vec<(usize, usize, usize, usize)> = Vec::with_capacity(50);
-    // for _ in 0..q {
-    //     abcd_list.push(parse_line().unwrap())
-    // }
-    // max_floor(n, m, q, abcd_list);
-     dbg!(pow(3, 3));
-     dbg!((3 as i32).pow(3));
-}
-
-// fn max_floor(n: usize, m: usize, q: usize, abcd_list: Vec<(usize, usize, usize, usize)>) -> i32 {
-//     dbg!(n, m, q, abcd_list);
-//     dbg!(pow(3, 3));
-//     10
-// }
-
-fn pow(i: usize, num: usize) -> usize {
-    private_pow(i, num, num)
-}
-// これができるんだったらクロージャーでいいんじゃないの
-fn private_pow(i: usize, num: usize, current: usize ) -> usize {
-    if i == 1 {
-        return num;
+    let x: i64 = parse_line().unwrap();
+    let mut num: i64 = 100;
+    let mut count: usize = 0;
+    loop {
+        num = (num / 100) + num;
+        count = count + 1;
+        if num >= x {
+            break;
+        }
     }
-    private_pow(i - 1, num * current, current)
+    println!("{}", count);
 }
